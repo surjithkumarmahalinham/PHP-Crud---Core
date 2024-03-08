f68fa7199c4157a2aaad2ddb71b495c0c9b365b7
surjithkumarmahalingam <40521122+surjithkumarmahalinham@users.noreply.github.com>
2024-03-08 14:06:33 +0530

    Add files via upload

---
+++ main.php
@@ -0,0 +1,31 @@
+Write a script to implement a simple calculator in php that performs addition, subtraction, multiplication and division.
+
+Create a script that simulates a simple inventory management system, it should allow user to add item, remove items and display the current inventory
+
+Write a script that reads a text file, counts the frequency of each word in the file, and displays the results.
+
+You will be judged based on the 
+1. Modularity of the code
+2. Usage of natively available functions
+3. Quality and clarity of the code
+4. Speed of solving the problem
+
+Earnings:                      Per mensum                 Per annum
+====================================================================
+Gross Pay (CTC)                 54,170                    6,50,040
+Basic                           27,085                    3,25,020
+HRA                             13,543                    1,62,516
+Flexipay***                     13,542                    1,62,504
+====================================================================
+Gross earnings (A)              54,170                    6,50,040
+====================================================================
+
+Deductions:                    Per mensum                 Per annum
+====================================================================
+PF contribution Employer         1,800                      21,600
+PF contribution Employee         1,800                      21,600
+Profession tax-*****               209                       2,508
+Other Deductions                    -                            -
+
+Gross Deductions (B)             3,809                      45,708
+Net Pay (A) - (B)               50,361                    6,04,332
